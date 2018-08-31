a8922c8d8ae17f8fd6644c74368a4558f115ef8d
张云翥 <zhangyunzhu@zhangyunzhudemacbook-pro.local>
2018-08-31 23:36:37 +0800

    计算型属性和懒加载的区别

--- 003-计算型属性/003-计算型属性/Person.swift
+++ 003-计算型属性/003-计算型属性/Person.swift
@@ -22,8 +22,21 @@ class Person: NSObject {
     
     var title :String{
         //只重写了getter方法没重写setter方法及为只读属性
+        //又称为计算型属性，本身不保存内容，都是通过计算获取结果
+        //计算型属性类似于一个函数，没有参数，一定有返回值
         return "Topus" + (name ?? "")
     }
+    
+    
+    //懒加载的titil，本质是一个闭包
+    //只在第一次调用的是后执行，后续调用会把结果保存在title2中，会分配空间
+    //非懒加载的每次调用都会计算
+    lazy var titil2:String = {
+    return "Lazy"+( name ?? "")
+    }()
+    
+    
+    
     
     
     
--- 003-计算型属性/003-计算型属性/ViewController.swift
+++ 003-计算型属性/003-计算型属性/ViewController.swift
@@ -10,24 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var p = Person()
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let p = Person()
+//        let p = Person()
         
         p.name = "老王"
         
+        
+        /*空合并运算符  (a ?? b) 将对可选类型a进行空判断，如果a包含一个值就进行解封，否则就返回一个默认值b。
+         两个条件：表达式a必须是可选类型，默认值b的类型必须要和a存储值的类型一致*/
         print(p.name ?? "")
         
        // p.title = "老李"
        // 'title' is a get-only property
         print(p.title)
+        print(p.titil2)
         
         
-        
-/*空合并运算符  (a ?? b) 将对可选类型a进行空判断，如果a包含一个值就进行解封，否则就返回一个默认值b。
- 两个条件：表达式a必须是可选类型，默认值b的类型必须要和a存储值的类型一致*/
-        
+    }
+    
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        p.name = "zhuzhu"
+        print(p.title)
+        print(p.titil2)//懒加载的不会改
     }
 
 
